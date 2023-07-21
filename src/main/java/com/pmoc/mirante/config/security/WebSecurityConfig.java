7fdc73e9e053208d9353eeeee4e222fa326be430
fymorGod <mufinagamer@gmail.com>
2023-07-21 15:20:23 -0300

    feat: insert new tables on project - tables of checklist and tarefas and tipo_equipamento - v1.5.12 :zap:

--- src/main/java/com/pmoc/mirante/config/security/WebSecurityConfig.java
+++ src/main/java/com/pmoc/mirante/config/security/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
     @Autowired
@@ -37,19 +38,16 @@ public class WebSecurityConfig {
     }
     @Bean
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
-        return http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests()
-                .requestMatchers("/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+         return http.csrf().disable()
+		                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				                 .and()
+                                 .addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
+                                 .authorizeHttpRequests()
+                                 .requestMatchers("/login").permitAll()
+                                 .anyRequest().authenticated()
+                                 .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                                 .build();
     }
-
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
         return configuration.getAuthenticationManager();
