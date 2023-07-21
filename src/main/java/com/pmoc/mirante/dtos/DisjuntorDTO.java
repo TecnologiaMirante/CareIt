1705225b735233d269f1d0787c1eb3adc06eacf9
fymorGod <mufinagamer@gmail.com>
2023-07-21 10:30:25 -0300

    feat: insert new tables on project - tables of checklist and tarefas and tipo_equipamento - v1.5.7 :zap:

--- src/main/java/com/pmoc/mirante/dtos/DisjuntorDTO.java
+++ src/main/java/com/pmoc/mirante/dtos/DisjuntorDTO.java
@@ -2,25 +2,93 @@ package com.pmoc.mirante.dtos;
 
 import com.pmoc.mirante.enums.Categories;
 import com.pmoc.mirante.enums.Status;
-import com.pmoc.mirante.models.checklist.TipoEquipamento;
-import com.pmoc.mirante.models.gerais.DadosGerais;
 import com.pmoc.mirante.models.gerais.Gerais;
 import com.pmoc.mirante.models.station.StationModel;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
-public record DisjuntorDTO(
+public class DisjuntorDTO {
+
         @NotNull
         @Valid
-        Gerais gerais,
-        int corrente_maxima,
-        @NotNull
-        Status status,
-        @NotNull
-        Categories category,
-        @NotNull
-        TipoEquipamento tipoEquipamento,
-        StationModel station
-) {
+        private Gerais gerais;
 
+        @NotNull
+        private int corrente_maxima;
+
+        @NotNull
+        private Status status;
+
+        @NotNull
+        private Categories category;
+
+        @NotNull
+        private Long tipoEquipamentoId; // Use o tipo adequado para representar o ID do TipoEquipamento
+
+        private Long stationId; // Use o tipo adequado para representar o ID da StationModel (se necessário)
+
+        // Construtor, getters e setters (ou você pode usar Lombok)
+
+        public DisjuntorDTO() {
+        }
+
+        public DisjuntorDTO(@NotNull @Valid Gerais gerais, @NotNull int corrente_maxima, @NotNull Status status,
+                            @NotNull Categories category, @NotNull Long tipoEquipamentoId, Long stationId) {
+                this.gerais = gerais;
+                this.corrente_maxima = corrente_maxima;
+                this.status = status;
+                this.category = category;
+                this.tipoEquipamentoId = tipoEquipamentoId;
+                this.stationId = stationId;
+        }
+
+        // getters e setters (ou você pode usar Lombok)
+
+        public Gerais getGerais() {
+                return gerais;
+        }
+
+        public void setGerais(Gerais gerais) {
+                this.gerais = gerais;
+        }
+
+        public int getCorrente_maxima() {
+                return corrente_maxima;
+        }
+
+        public void setCorrente_maxima(int corrente_maxima) {
+                this.corrente_maxima = corrente_maxima;
+        }
+
+        public Status getStatus() {
+                return status;
+        }
+
+        public void setStatus(Status status) {
+                this.status = status;
+        }
+
+        public Categories getCategory() {
+                return category;
+        }
+
+        public void setCategory(Categories category) {
+                this.category = category;
+        }
+
+        public Long getTipoEquipamentoId() {
+                return tipoEquipamentoId;
+        }
+
+        public void setTipoEquipamentoId(Long tipoEquipamentoId) {
+                this.tipoEquipamentoId = tipoEquipamentoId;
+        }
+
+        public Long getStationId() {
+                return stationId;
+        }
+
+        public void setStationId(Long stationId) {
+                this.stationId = stationId;
+        }
 }
